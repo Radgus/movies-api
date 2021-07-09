717d7ba81fa8a08b13f07478548da20229ca2390
LuisD <dominguezradgus@gmail.com>
2021-07-08 19:27:54 -0500

    data validation layer added

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
+app.use(express.json());
+
+// routes
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
